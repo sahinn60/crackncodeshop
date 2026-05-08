a34be73670ae8148a7e4225c03f786c72ec1f457
sahinn60 <crackncode47@gmail.com>
2026-05-08 22:46:40 +0600

    fix: rewrite eslint flat config - remove FlatCompat circular reference crash

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,29 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextPlugin from "@next/eslint-plugin-next";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
   {
+    ignores: ["server.js", "app.js", "test-server.js", ".next/**", "node_modules/**"],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@next/next": nextPlugin,
+      "@typescript-eslint": tsPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-unused-vars": "off",
-      "react-hooks/exhaustive-deps": "off",
       "@next/next/no-img-element": "off",
     },
   },
-  {
-    ignores: ["server.js", "app.js", "test-server.js"],
-  },
 ];
-
-export default eslintConfig;
